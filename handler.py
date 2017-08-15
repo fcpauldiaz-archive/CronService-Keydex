af42247efd62ae9e81469f7b8dda3c1efbc17e30
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-08-15 14:46:52 -0600

    cron algorithm v1

--- handler.py
+++ handler.py
@@ -2,10 +2,22 @@
 import json
 import psycopg2
 from scraper.crawler import begin_crawl
-from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
+from calendar import monthrange
+import logging
+import datetime
 import requests
 import os
+
+def last_day_month(today):
+    dict_range = monthrange(today.year, today.month)
+    if dict_range[1] == today.day:
+        return True
+    return False
+def weekend(today):
+    if today.weekday() == 6:
+        return True
+    return False
 
 def get_user_data(product_id, cursor):
     query = '''
@@ -57,7 +69,7 @@ def save_product_indexing(result, product_id, cursor, conn):
         (id, keyword, indexing, index_date, product_id)
         VALUES(nextval('main_keywords_id_seq'::regclass), %s, %s, %s, %s);
     '''
-    data = (keyword, indexing, datetime.now(), product_id)
+    data = (keyword, indexing, datetime.datetime.now(), product_id)
     cursor.execute(query, data)
     conn.commit()
 
@@ -71,15 +83,14 @@ def save_product_indexing(result, product_id, cursor, conn):
     (id, indexing_rate, indexed_date, product_id)
     VALUES(nextval('main_product_historic_indexing_id_seq'::regclass), %s, %s, %s);
   '''
-  data = (indexing_rate, datetime.now(), product_id)
+  data = (indexing_rate, datetime.datetime.now(), product_id)
   #save transactional operation
   cursor.execute(query, data)
   conn.commit()
   return indexing_rate
 
 
-def hello():
-
+def hello(event, context):
     # Connect to an existing database
     if 'DB_HOST' in os.environ:
       conn = psycopg2.connect(
@@ -109,7 +120,7 @@ def hello():
     rows = cur.fetchall()
     #asin, keywords, reporting_percentage, periodicity
     
-
+    failed = False
     for row in rows:
         try:
             product_id = row[0]
@@ -119,35 +130,48 @@ def hello():
             periodicity = row[4]
             country_code = row[5]
             country_host = row[6]
+            today = datetime.date.today()
+            if (periodicity == 'monthly'):
+                #check if today is endof month
+                monthly = last_day_month(today)
+                if (monthly == False):
+                    continue
+            elif (periodicity == 'weekly'):
+                #check if today is sunday
+                sunday = weekend(today)
+                if (sunday == False):
+                    continue
+            elif (periodicity == '-1'):
+                continue
             rDict = begin_crawl(asin, keywords, country_host, country_code)
             rate = save_product_indexing(rDict, product_id, cur, conn)
-            
             if reporting_percentage >= 100:
                 first_name, last_name, email_to = get_user_data(product_id, cur)
                 #send email
-                send_email(asin, first_name, last_name, keywords, rate, email_to)
+                send_email(asin, first_name, last_name, keywords, format(rate, '.2f'), email_to)
             elif reporting_percentage >= rate:
                 first_name, last_name, email_to = get_user_data(product_id, cur)
                 #send email
-                send_email(asin, first_name, last_name, keywords, rate, email_to)
+                send_email(asin, first_name, last_name, keywords, format(rate, '.2f'), email_to)
         except:
-            #continue for next product
+            logger.error('something went wrong on product {}'.format(row[0]))
+            failed = True
             pass
         
     cur.close()
     conn.close()
 
-    # body = {
-    #     "message": asins,
-    #     "input": event
-    # }
+    body = {
+        "message": failed,
+        "input": event
+    }
 
-    # response = {
-    #     "statusCode": 200,
-    #     "body": json.dumps(body)
-    # }
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
 
-    # return response
+    return response
 
     # Use this code if you don't use the http event with the LAMBDA-PROXY integration
     """
@@ -156,4 +180,3 @@ def hello():
         "event": event
     }
     """
-hello()
